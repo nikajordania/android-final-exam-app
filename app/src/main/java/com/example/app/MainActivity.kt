14dfb2b635ec1cab349a51915d871e48a9c9d27e
Nika Zhordania <nika.zhordania.1@btu.edu.ge>
2022-06-21 00:57:35 +0400

    add: back-end OpenWeatherMapService and WeatherRecyclerAdapter

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,76 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.app.api.RestClient
+import com.example.app.api.dto.onecall.OneCall
+import com.example.app.api.dto.weatherdata.WeatherData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cities = listOf(
+            "Tbilisi",
+            "Batumi",
+            "Kutaisi",
+            "Rustavi",
+            "London",
+            "Hawaii",
+//            "Gori",
+//            "Zugdidi",
+//            "Poti",
+//            "Kobuleti",
+//            "Khasuri",
+//            "Samtredia"
+        )
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        val x = mutableListOf<WeatherData>()
+
+        RestClient.initClient()
+        for (city in cities) {
+            RestClient.openWeatherMapService.getWeather(
+                q = city,
+                units = "metric",
+                appid = "03006558897f1413ecaaf87f7cadec03"
+            ).enqueue(object : Callback<WeatherData> {
+                override fun onResponse(
+                    call: Call<WeatherData>, response: Response<WeatherData>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { x.add(it) }
+                        Log.d("Successful: " + city, response.body().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<WeatherData>, t: Throwable) {
+                    Log.d("Failure", t.message.toString())
+                }
+            })
+        }
+
+
+//        val oneCall: OneCall? = RestClient.openWeatherMapService.getOneCall(
+//            lat = 33.44,
+//            lon = -94.04,
+//            exclude = "hourly,daily",
+//            units = "metric",
+//            appid = "03006558897f1413ecaaf87f7cadec03"
+//        ).execute().body()
+//
+//        Log.d("Onecall", oneCall.toString())
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = WeatherRecyclerAdapter(x)
+        }
     }
 }
---
+++ app/src/main/java/com/example/app/WeatherRecyclerAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.app
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.app.api.dto.weatherdata.WeatherData
+import com.squareup.picasso.Picasso
+
+
+class WeatherRecyclerAdapter(private val weatherListData: MutableList<WeatherData>) :
+    RecyclerView.Adapter<WeatherRecyclerAdapter.UserViewHolder>() {
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val weatherIcon: ImageView = itemView.findViewById(R.id.weatherIcon)
+        private val weatherView: TextView = itemView.findViewById(R.id.weather)
+        private val city: TextView = itemView.findViewById(R.id.city)
+        private val humidity: TextView = itemView.findViewById(R.id.humidity)
+        private lateinit var weatherData: WeatherData
+
+
+        @SuppressLint("SetTextI18n")
+        fun onBind(weatherData: WeatherData) {
+            Picasso.get().load(getIcon(weatherData.weather[0].icon)).into(weatherIcon)
+            weatherView.text = weatherData.main.temp.toInt().toString() + "℃"
+            city.text = weatherData.name
+            humidity.text = weatherData.main.humidity.toString() + "%"
+            this.weatherData = weatherData
+        }
+
+        private fun getIcon(id: String): String {
+            return "https://openweathermap.org/img/wn/$id@4x.png"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_weather_recycler, parent, false)
+
+        return UserViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.onBind(weatherListData[position])
+    }
+
+    override fun getItemCount() = weatherListData.size
+}
